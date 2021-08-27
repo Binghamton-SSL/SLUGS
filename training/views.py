92d6da27827a52d7d7a095feac0d55606b7a7e0d
Joe Babbitt <jrebbabbitt@gmail.com>
2021-08-27 15:13:03 -0400

    reset for 2.0

--- training/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    if not request.user.is_authenticated:
-        return redirect('%s?next=%s' % (reverse('auth:login'), request.path))
-    return render(request, 'training/index.html', {})
