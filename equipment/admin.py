e5708144bef5cbbd8452440c6986c0c93e9fe440
Kenechi Okoye <okoyekk@gmail.com>
2022-01-22 14:51:48 -0500

    Created models for Equipment Management Feature (#26)
    Added models for equipment inventory.
    Co-authored-by: Joe Babbitt <jrebbabbitt@gmail.com>

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,21 +1,40 @@
 from django.contrib import admin
-from equipment.models import System, BrokenEquipmentReport, SystemAddon
+from equipment.models import System, BrokenEquipmentReport, SystemAddon, Equipment, Category, ServiceRecord, Item, SystemQuantity, SystemQuantityAddon
 from finance.admin import SystemAddonPricingInline, SystemPricingInline
 
 
-@admin.register(SystemAddon)
-class SystemAddon(admin.ModelAdmin):
-    inlines = [SystemAddonPricingInline]
-    search_fields = ["name"]
-    pass
-
-
 # Register your models here.
+class ServiceRecordInline(admin.StackedInline):
+    model = ServiceRecord
+    readonly_fields = ['date_created', 'date_last_modified']
+    extra = 0
+
+
+class ItemInline(admin.StackedInline):
+    model = Item
+    extra = 0
+
+
+class EquipmentInline(admin.StackedInline):
+    model = SystemQuantity
+    extra = 0
+
+
+class EquipmentAddonInline(admin.StackedInline):
+    model = SystemQuantityAddon
+    extra = 0
+
+
+@admin.register(SystemAddon)
+class SystemAddonAdmin(admin.ModelAdmin):
+    inlines = [SystemAddonPricingInline, EquipmentAddonInline]
+    search_fields = ["name"]
+
+
 @admin.register(System)
 class SystemAdmin(admin.ModelAdmin):
-    inlines = [SystemPricingInline]
+    inlines = [SystemPricingInline, EquipmentInline]
     search_fields = ["name"]
-    pass
 
 
 @admin.register(BrokenEquipmentReport)
@@ -31,3 +50,25 @@ class BrokenEquipmentReportAdmin(admin.ModelAdmin):
         ),
         (None, {"fields": ("investigation",)}),
     ]
+
+
+@admin.register(Equipment)
+class Equipment(admin.ModelAdmin):
+    inlines = [ItemInline]
+    search_fields = ["name", "brand", "model_number", "reorder_link"]
+
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(ServiceRecord)
+class ServiceRecord(admin.ModelAdmin):
+    search_fields = ["name", "date_created"]
+
+
+@admin.register(Item)
+class Item(admin.ModelAdmin):
+    inlines = [ServiceRecordInline]
+    search_fields = ["id"]
--- equipment/models.py
+++ equipment/models.py
@@ -3,15 +3,48 @@ from django.db import models
 from gig.models import DEPARTMENTS
 import employee.models as employee
 from django.db.models import Q
+from datetime import date
 
 from utils.models import PricingMixin
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+         verbose_name_plural = "Categories"
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1024, blank=True, null=True)
+    brand = models.CharField(max_length=200, blank=True, null=True)
+    model_number = models.CharField(max_length=200, blank=True, null=True)
+    department = models.CharField(max_length=1, choices=DEPARTMENTS)
+    value = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    wattage = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    purchase_date = models.DateField(default=date.today, help_text="Date of purchase or creation if made")
+    reorder_link = models.URLField(blank=True)
+
+    def __str__(self):
+        if not self.brand:
+            return f"{self.name}"
+
+        return f"{self.name} ({self.brand})"
+
+    class Meta:
+         verbose_name_plural = "Equipment"
+
+
 class System(PricingMixin, models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1024, blank=True, null=True)
     department = models.CharField(max_length=1, choices=DEPARTMENTS)
+    equipment = models.ManyToManyField(Equipment, through="SystemQuantity")
 
     def __init__(self, *args, **kwargs):
         self.pricing_set = self.systempricing_set
@@ -51,3 +84,54 @@ class BrokenEquipmentReport(models.Model):
 
     def __str__(self):
         return self.status + " - " + str(self.broken_system)
+
+
+# new models
+class Item(models.Model):
+    serial_no = models.CharField(max_length=200)
+    item_type = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.item_type} #{self.serial_no}"
+
+
+class ServiceRecord(models.Model):
+    name = models.CharField(max_length=200)
+    date_created = models.DateField(auto_now_add=True)
+    date_last_modified = models.DateField(auto_now=True)
+    note = models.TextField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} on {self.date_created}"
+
+
+class BaseQuantity(models.Model):
+    """Through model linking System and Equipment quantity"""
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.equipment}, Quantity: {self.quantity}, "
+
+
+class SystemQuantity(BaseQuantity):
+    system = models.ForeignKey(System, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Equipment Quantity"
+        verbose_name_plural = "Equipment Quantities"
+
+    def __str__(self):
+        return super().__str__() + f"System: {self.system}"
+
+
+class SystemQuantityAddon(BaseQuantity):
+    system_addon = models.ForeignKey(SystemAddon, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Addon Equipment Quantity"
+        verbose_name_plural = "Addon Equipment Quantities"
+
+    def __str__(self):
+        return super().__str__() + f"System: {self.system}"
